d0cc2855207a9aa3f46b72d477062e1d403b04c8
Pedro <ppeck@fusap.com.ar>
2023-04-03 23:37:11 -0300

    Se agregan test para model Crypto

--- src/main/java/ar/edu/unq/grupok/backenddesappapi/model/Crypto.java
+++ src/main/java/ar/edu/unq/grupok/backenddesappapi/model/Crypto.java
@@ -1,35 +1,36 @@
 package ar.edu.unq.grupok.backenddesappapi.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Crypto {
 
-	private Double price;
-	
 	private String symbol;
+
+	private Double price;
 	
 	private LocalDateTime dateTimeOfLastPrice;
 	
 	private Map<LocalDateTime, Double> priceHistory;
 	
-	public Crypto(Double price, String symbol) {
+	public Crypto(String symbol, Double price) {
 		
 		super();
-		this.price = price;
 		this.symbol = symbol;
+		this.price = price;
 		this.dateTimeOfLastPrice = LocalDateTime.now();
 		this.priceHistory = new HashMap<LocalDateTime, Double>();
 		this.priceHistory.put(dateTimeOfLastPrice, price);
 	}
 
-	public Double getPrice() {
-		return price;
-	}
-
 	public String getSymbol() {
 		return symbol;
+	}
+
+	public Double getPrice() {
+		return price;
 	}
 	
 	public void setPrice(Double price) {
@@ -40,7 +41,7 @@ public class Crypto {
 	public Map<LocalDateTime, Double> pricesOfTheLast24Hours(){
 		HashMap<LocalDateTime, Double> last24Hours = new HashMap<>();
 		LocalDateTime twentyFourHoursAgo = LocalDateTime.now().minusHours(24);
-		
+
 		for (var entry : this.priceHistory.entrySet()) {
 		    if(entry.getKey().isAfter(twentyFourHoursAgo)) {
 		    	last24Hours.put(entry.getKey(), entry.getValue());
--- src/test/java/ar/edu/unq/grupok/backenddesappapi/BackendDesappApiApplicationTests.java
+++ src/test/java/ar/edu/unq/grupok/backenddesappapi/BackendDesappApiApplicationTests.java
@@ -1,13 +1,62 @@
 package ar.edu.unq.grupok.backenddesappapi;
 
+import ar.edu.unq.grupok.backenddesappapi.model.Crypto;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.*;
+
 @SpringBootTest
 class BackendDesappApiApplicationTests {
+	private Crypto aRandomCrypto = anyCrypto();
+
+	private @NotNull Crypto anyCrypto() {
+		Crypto aCrypto = new Crypto("ALICEUSDT", 1.574);
+		return aCrypto;
+	}
+
+//	@BeforeAll
+//	public void contextLoads() {
+//	}
+
+	// CRYPTO UNIT TESTS
+	@Test
+	void createNewCryptoAndCheckSymbol() {
+		String symbol = "MATICUSDT";
+		Crypto aCrypto = new Crypto(symbol, 0.0);
+		Assertions.assertEquals(aCrypto.getSymbol(),symbol);
+	}
 
 	@Test
-	void contextLoads() {
+	void modifyCryptoPrice() {
+		Double newCryptoPrice = 1.600;
+		aRandomCrypto.setPrice(newCryptoPrice);
+		Assertions.assertEquals(aRandomCrypto.getPrice(),newCryptoPrice);
+	}
+
+	@Test
+	void createCryptoAndCheckPricesHistoryIncrementation() {
+		Double cryptoFirstPrice = 308.5;
+		Crypto aCrypto = new Crypto("AXSUSDT", cryptoFirstPrice);
+		Collection<Double> last24HoursPricesValues = aCrypto.pricesOfTheLast24Hours().values();
+		Assertions.assertEquals(last24HoursPricesValues.size(),1);
+
+		aCrypto.setPrice(309.0);
+		last24HoursPricesValues = aCrypto.pricesOfTheLast24Hours().values();
+		Assertions.assertEquals(last24HoursPricesValues.size(),2);
+	}
+
+	@Test
+	void createCryptoAndSetSamePriceTwoTimes() {
+		Double cryptoFirstPrice = 308.5;
+		Crypto aCrypto = new Crypto("AXSUSDT", cryptoFirstPrice);
+		Collection<Double> last24HoursPricesValues = aCrypto.pricesOfTheLast24Hours().values();
+		aCrypto.setPrice(309.0);
+		aCrypto.setPrice(309.0);
+		last24HoursPricesValues = aCrypto.pricesOfTheLast24Hours().values();
+		Assertions.assertEquals(last24HoursPricesValues.size(),3);
 	}
 
 }
