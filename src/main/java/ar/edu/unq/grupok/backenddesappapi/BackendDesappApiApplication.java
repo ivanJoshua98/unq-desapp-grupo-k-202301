b26d8d708c62e9710447428ab33f7667118ba316
unknown <ivan.sanmartin98@gmail.com>
2023-06-07 20:49:55 -0300

    Se modifica clase run

--- src/main/java/ar/edu/unq/grupok/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/grupok/backenddesappapi/BackendDesappApiApplication.java
@@ -3,12 +3,14 @@ package ar.edu.unq.grupok.backenddesappapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@Configuration
 @EnableScheduling
 @OpenAPIDefinition(info = @Info(title = "CriptoP2P", 
 								version = "1.0", 
